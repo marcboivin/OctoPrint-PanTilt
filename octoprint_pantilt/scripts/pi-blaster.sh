4f24601bc5de0348e6b6f012479dec61281c0400
Marc Boivin <mboivin@Marcs-MacBook-Pro-2.local>
2018-02-14 11:03:27 -0500

    Hacked angle to second transformation into the pi-blaster script

--- octoprint_pantilt/scripts/pi-blaster.sh
+++ octoprint_pantilt/scripts/pi-blaster.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 
-echo "17=$1 18=$2" > /dev/pi-blaster
+pan=$(python -c "print $1/100")
+pan=$(python -c "print $pan+0.12")
+
+tilt=$(python -c "print $1/100")
+tilt=$(python -c "print $tilt+0.12")
+
+echo "17=$pan 18=$tilt" > /dev/pi-blaster
